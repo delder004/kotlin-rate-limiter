03c0b7e3c0f21a07ec5845ff90460c10a2c476ca
Delos Elder <dmelder@gmail.com>
2026-04-07 08:50:53 -1000

    publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     kotlin("jvm") version "2.2.20"
     id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
+    id("com.vanniktech.maven.publish") version "0.30.0"
 }
 
-group = "org.soled"
-version = "1.0-SNAPSHOT"
+group = "io.github.delder004"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
@@ -22,4 +23,32 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+mavenPublishing {
+    publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+
+    pom {
+        name.set("kotlin-rate-limiter")
+        description.set("Coroutine-native, client-side rate limiter for Kotlin")
+        url.set("https://github.com/delder004/kotlin-rate-limiter")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("delder004")
+                name.set("Delos Elder")
+            }
+        }
+        scm {
+            url.set("https://github.com/delder004/kotlin-rate-limiter")
+            connection.set("scm:git:git://github.com/delder004/kotlin-rate-limiter.git")
+            developerConnection.set("scm:git:ssh://github.com/delder004/kotlin-rate-limiter.git")
+        }
+    }
 }
