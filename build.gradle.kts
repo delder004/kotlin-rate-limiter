6cf29364b8b2cde13abd749be2fb1f4fd2303cc4
Delos Elder <dmelder@gmail.com>
2026-04-07 13:36:52 -1000

    refactor: version cat and build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "2.2.20"
-    id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
-    id("com.vanniktech.maven.publish") version "0.30.0"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.maven.publish)
 }
 
 group = "io.github.delder004"
@@ -12,10 +12,10 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
-    testImplementation(kotlin("test"))
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.4")
+    implementation(libs.kotlinx.coroutines.core)
+    testImplementation(libs.kotlin.test)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.junit.jupiter.params)
 }
 
 tasks.test {
@@ -23,32 +23,4 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
-}
-
-mavenPublishing {
-    publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL)
-    signAllPublications()
-
-    pom {
-        name.set("kotlin-rate-limiter")
-        description.set("Coroutine-native, client-side rate limiter for Kotlin")
-        url.set("https://github.com/delder004/kotlin-rate-limiter")
-        licenses {
-            license {
-                name.set("The Apache License, Version 2.0")
-                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-            }
-        }
-        developers {
-            developer {
-                id.set("delder004")
-                name.set("Delos Elder")
-            }
-        }
-        scm {
-            url.set("https://github.com/delder004/kotlin-rate-limiter")
-            connection.set("scm:git:git://github.com/delder004/kotlin-rate-limiter.git")
-            developerConnection.set("scm:git:ssh://github.com/delder004/kotlin-rate-limiter.git")
-        }
-    }
 }
